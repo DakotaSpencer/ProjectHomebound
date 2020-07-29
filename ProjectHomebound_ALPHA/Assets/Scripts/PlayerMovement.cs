5a7f29d67822f3ad9c86e96b0182a4862eb379be
DakotaSpencer <dakota.spencer0327@gmail.com>
2020-07-29 19:27:33 -0400

    Fixed movement script
    Fixed player movement script and rerigged the camera controller using Cinemachine. Also added collision detection so the camera should smoothly move towards the player if the line of sight is obstructed by the environment.

--- ProjectHomebound_ALPHA/Assets/Scripts/PlayerMovement.cs
+++ ProjectHomebound_ALPHA/Assets/Scripts/PlayerMovement.cs
@@ -12,11 +12,13 @@ public class PlayerMovement : MonoBehaviour
     
     // Public floats
     public float speed = 6f;  // Speed of the player
-    public float turningTime = 0.1f;  // Time of the player turning
+    public float turnSmoothingTime = 0.1f;  // Time of the player turning
     public float groundDistance = 0.4f;  // Ground distance
     public float gravity = -9.81f; // Velocity of gravity
     public float jumpHeight = 3f; // How high the player jumps
-    public float sprintAccel = 1.75f; // How fast the player acelerate
+
+    public float WalkSpeed = 6f; //How fast the player accelerates **WHEN WALKING**
+    public float RunSpeed = 12f; // How fast the player acelerates **WHEN RUNNING**
 
     // Private variables
     Vector3 velocity; // Calls for velocity of falling
@@ -26,55 +28,56 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        speed = Input.GetKey(KeyCode.LeftShift) ? RunSpeed : WalkSpeed;//Allows to switch between walk and run
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;//hides Cursor when in play mode. Press ESC t release cursor
+        
+        // Checks Ground Check to see if player is on the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
         float horizontal = Input.GetAxisRaw("Horizontal"); // A/D or Left/Right
         float vertical = Input.GetAxisRaw("Vertical"); // W/S or Up/Down
         // Make sures that we don't add upon values (when two keys are pressed) and moves along x- and z-axises
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        // Detects if WASD/Arrow keys are pressed
-        if (direction.magnitude >= 0.1f)
-        {
-            // Creates a new angle for the player to follow.
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turningTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            // Moves the player in the angle that the camera is facing.
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-            // Allows the player to sprint
-            // TODO: add the X button sprinting.
-            if (Input.GetKey("left shift"))
-            {
-                speed = sprintAccel * speed;
-                controller.Move(moveDir.normalized * speed * Time.deltaTime);
-            }
-            else
-            {
-                speed = 6f;
-                controller.Move(moveDir.normalized * speed * Time.deltaTime);
-            }
-
-            //TODO: Add crouching
-        }
-
-        // Checks Ground Check to see if player is on the ground
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        
         //Makes sure that the falling velocity does not go under -2.
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
+        
+        // Allows the player to fall.
+        velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(velocity * Time.deltaTime);
 
         // Allows the player to jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+
+        // Allows the player to sprint
+        if (Input.GetButtonDown("Sprint") && isGrounded)
+        {
+            Vector3 sprintDirection = new Vector3(horizontal, 0f, vertical).normalized * 2;
+        }
         
-        // Allows the player to fall.
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        // Detects if WASD/Arrow keys are pressed
+        if (direction.magnitude >= 0.1f)
+        {
+            // Creates a new angle for the player to follow.
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothingTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Moves the player in the angle that the camera is facing.
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            // TODO: add the X button sprinting.
+            //TODO: Add crouching
+        }
+        
     }
 }
