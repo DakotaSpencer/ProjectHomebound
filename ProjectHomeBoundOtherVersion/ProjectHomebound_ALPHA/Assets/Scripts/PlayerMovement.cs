28aa4192f50003fe844b8cbcbe09b698550bc751
akshatsinghania <62429864+akshatsinghania@users.noreply.github.com>
2020-07-31 22:33:44 +0530

    Added transsition

---
+++ ProjectHomeBoundOtherVersion/ProjectHomebound_ALPHA/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//////Build upon this and try not to change anything, unless absolutely necessary.//////
+public class PlayerMovement : MonoBehaviour
+{
+    // Public variables in Unity
+    public CharacterController controller;  // Calls CharacterController
+    public Transform cam; // Calls for Main Camera controlled by Cinemachine
+    public Transform groundCheck; // Calls for the ground check
+    public LayerMask groundMask; // Calls for the ground layer
+    
+    // Public floats
+    public float speed = 6f;  // Speed of the player
+    public float turnSmoothingTime = 0.1f;  // Time of the player turning
+    public float groundDistance = 0.4f;  // Ground distance
+    public float gravity = -9.81f; // Velocity of gravity
+    public float jumpHeight = 3f; // How high the player jumps
+
+    public float WalkSpeed = 6f; //How fast the player accelerates **WHEN WALKING**
+    public float RunSpeed = 12f; // How fast the player acelerates **WHEN RUNNING**
+
+    public float CrouchSpeed = 2f;
+    public float NormalHeight = 1f;
+    public float CrouchHeight = 0.5f;
+
+    // Private variables
+    Vector3 velocity; // Calls for velocity of falling
+    float turnSmoothVelocity; // Velocity for smoother turning
+    bool isGrounded; // Ask is player is on the ground
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = Input.GetKey(KeyCode.LeftShift) ? RunSpeed : WalkSpeed;//Allows to switch between walk and run
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;//hides Cursor when in play mode. Press ESC t release cursor
+        
+        // Checks Ground Check to see if player is on the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        float horizontal = Input.GetAxisRaw("Horizontal"); // A/D or Left/Right
+        float vertical = Input.GetAxisRaw("Vertical"); // W/S or Up/Down
+        // Make sures that we don't add upon values (when two keys are pressed) and moves along x- and z-axises
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        
+        //Makes sure that the falling velocity does not go under -2.
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        // Allows the player to fall.
+        velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(velocity * Time.deltaTime);
+
+        // Allows the player to jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //Added and updated crouching 7/30/2020 ~ Dakota
+        if (Input.GetKey(KeyCode.LeftControl))  // Might need to configure in Input Manager 
+        {
+            controller.height = CrouchHeight;
+            speed = Input.GetKey(KeyCode.LeftControl) ? CrouchSpeed : WalkSpeed;
+        }
+        else
+        {
+            controller.height = NormalHeight;
+            //speed = speed;
+        }
+
+        // Detects if WASD/Arrow keys are pressed
+        if (direction.magnitude >= 0.1f)
+        {
+            // Creates a new angle for the player to follow.
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothingTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Moves the player in the angle that the camera is facing.
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            //TODO: Add crouching
+        }
+        
+    }
+}
