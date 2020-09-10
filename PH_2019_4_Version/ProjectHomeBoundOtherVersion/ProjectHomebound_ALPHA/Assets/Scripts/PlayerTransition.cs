4d590c743033b6b72828b6ab7f77d53bb33d63cd
Peter Ouzts <petterouztts@gmail.com>
2020-09-09 18:29:13 -0600

    Merge branch 'master' of https://github.com/DakotaSpencer/ProjectHomebound
    please work stupid software!!

---
+++ PH_2019_4_Version/ProjectHomeBoundOtherVersion/ProjectHomebound_ALPHA/Assets/Scripts/PlayerTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTransition : MonoBehaviour
+{ 
+    
+    
+    
+    void OnCollisionEnter(Collision collision)
+    {
+ 
+        if(collision.gameObject.name == "Collider")
+        {
+            Debug.Log("Load dogs scene");
+        }
+        
+    }
+}
